348d9e1b835f23ca55eb80d2deec58bd49d1508f
Alexandre Van <avan@c1r2p9.42nice.fr>
2023-06-16 19:20:04 +0200

    debut d'exec

--- srcs/exec.c
+++ srcs/exec.c
@@ -18,38 +18,57 @@ static void ft_child_process(t_struct *s)
 		dup2()
 }
 
-static void ft_pipe_and_fork(t_struct *s)
+static void ft_pipe_and_fork(t_struct *s, t_parsed *parsed)
 {
-	if (!s || s->i_cmd >= s->nb_cmd)
-		return;
-	if (s->i_cmd < s->nb_cmd)
+	if (!s || !(parsed->command))
+		return ;
+	if (!(parsed->prev))
 	{
-		s->parsed->pipe_fd = malloc(sizeof(int) * 2);
-		if (!(s->parsed->pipe_fd))
+		s->pipe_fd = malloc(sizeof(int) * 2);
+		if (!(s->pipe_fd))
 			return (ft_error(MALLOC, "malloc"));
-		if (pipe(s->parsed->pipe_fd) < 0)
-			return (ft_error(PIPE, "pipe"));
+	}
+	if (parsed->next)
+	{
+		if (pipe(s->pipe_fd) < 0)
+			return (ft_error(s, PIPE, "pipe"));
 	}
 	s->parsed->pid = fork();
 	if (s->parsed->pid < 0)
-		return (ft_error(FORK, "fork"));
+	{
+		perror("fork");
+		exit(1);
+	}
 	if (s->parsed->pid == 0)
 		ft_child_process(s);
+	close(s->pipe_fd[1]);
+	close(s->previous_fd);
+	if (parsed->next)
+		dup2(s->pipe_fd[0], s->previous_fd);
+	close(s->pipe_fd[0]);
+	if (!(parsed->next))
+		ft_wait_all_processes(s);
+	if (parsed->here_d_pipe_fd)
+	{
+		close(parsed->pipe_fd[0]);
+		fr_free_hd(parsed);
+	}
 	if (s->i_cmd > 0 && s->parsed->next)
 		close(s->parsed->pipe_fd[0]);
 	close(s->parsed->pipe_fd[1]);
-	s->i_cmd += 1;
-	if (s->i_cmd == s->nb_cmd)
-		ft_wait_all_processes(s);
-	/*  Gerer Here_doc << */
 }
 
 void ft_exec(t_struct *s)
 {
+	t_parsed	*parsed;
 	if (!s)
-		return;
+		return ;
 	//    ft_get_commands(s);
 	ft_open_files_get_fds(s);							// open all files and get fds
-	while (s->i_cmd < s->nb_cmd)
-		ft_pipe_and_fork(s);
+	parsed = s->parsed;
+	while (parsed)
+	{
+		ft_pipe_and_fork(s, parsed);
+		parsed = parsed->next;
+	}
 }
